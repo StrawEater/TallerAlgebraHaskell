aa13740de810d8b70d5cb9ba20f5e414438ce190
StrawEater <59102276+StrawEater@users.noreply.github.com>
2022-04-08 16:25:08 -0300

    Mejora del Graficador

--- Clase3/Clase3.hs
+++ Clase3/Clase3.hs
@@ -1,5 +1,5 @@
 llenarLinea n | n > 0 = (putStr "d") >> (llenarLinea (n-1))
-                | otherwise = (putStrLn "dou")
+                | otherwise = (putStrLn "")
 
 
 prueba:: Int -> Float -> Float -> IO()
@@ -7,4 +7,21 @@ prueba n thi max | n > 0 = (llenarLinea length) >> (prueba (n-1) (thi + 0.1) max
                  | otherwise = (llenarLinea n)
                  where length = (round (max * ((cos thi)+1)))
 
- 
+
+--Poner Igualdad aca
+getFull:: Float -> Float -> Bool
+--getFull x y = y >= ((x-25)**2)/10
+--getFull x y = x <= ((cos y) + 1) * 20
+getFull x y = (y - 20)**2 + (x - 20)**2 <= (15)**2 
+
+--
+lineaGraf:: Float -> Float -> Float -> IO ()
+lineaGraf n y xMax | (n < xMax && (getFull n y)) = (putStr "d") >> (lineaGraf (n+1) y xMax)
+                   | (n < xMax && (not (getFull n y))) = (putStr " ") >> (lineaGraf (n+1) y xMax)
+                   | otherwise = (putStrLn "")
+
+--Llamar a esta funcion para Graficar
+graf:: Float -> Float -> Float -> IO ()
+graf xMax yMax y | yMax > y = (lineaGraf 0 y xMax) >> (graf xMax yMax (y+2))
+                 | otherwise = (llenarLinea xMax)
+                 
